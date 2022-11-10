04b53fedffde109e068c130e5c590d6cba975c56
Dinh Le <dinhledev@gmail.com>
2022-11-09 20:11:17 -0500

    force login,and update sql for student page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -41,6 +41,8 @@ function Home() {
       if (response.data.loggedIn == true) {
         setLoginUser(response.data.user[0].username);
         setData(response.data);
+      } else {
+        history.push("/login");
       }
     });
   }, []);
